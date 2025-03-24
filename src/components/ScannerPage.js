db082088ccfd850b28df635ee1ad5772f1c0be80
Sushobhit918 <156120488+Sushobhit918@users.noreply.github.com>
2025-03-24 15:31:50 +0530

    Update ScannerPage.js

--- src/components/ScannerPage.js
+++ src/components/ScannerPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ScannerPage() {
   const openFlaskModel = () => {
-    window.open("https://7e50-136-233-11-130.ngrok-free.app", "_blank"); // Opens Flask app in a new tab
+    window.open("http://127.0.0.1:5000/", "_blank"); // Opens Flask app in a new tab
   };
 
   return (
